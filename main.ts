38a1a47ad36c9a142f0e4c9059cbe85bbf51d846
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-05-03 15:01:24 -0400

    Created a variable that stores an object

--- main.ts
+++ main.ts
@@ -5,3 +5,9 @@ let numberOfCupsOfCoffee = 4
 const fullName = 'Fernando Aguilar'
 
 console.log(fullName, numberOfCupsOfCoffee)
+
+const aboutMe = {
+  fullName: 'Fernando J.V. Aguilar',
+  luckyNumber: 16,
+  favoriteMovie: 'Avatar, Everything Everywhere All at Once, Nope, RRR',
+}
