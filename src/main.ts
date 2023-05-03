f11b1e80e478c0456deb37e25f217cac07028edc
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-05-03 19:00:07 -0400

    Adventure mode completed up to Doing Math portion

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,46 @@ import './style.css'
 // `
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+//Practice Creating Variables
+let numberOfCupsOfCoffee = 4
+
+const fullName = 'Fernando Aguilar'
+
+console.log(fullName, numberOfCupsOfCoffee)
+
+const aboutMe = {
+  fullName: 'Fernando J.V. Aguilar',
+  luckyNumber: 16,
+  favoriteMovie: 'Avatar, Everything Everywhere All at Once, Nope, RRR',
+}
+
+function greeting() {
+  return 'Hello ' + userName + '!'
+}
+
+//Practice Getting Input From the User
+const userName = window.prompt('What is your user name?')
+console.log(greeting)
+
+//Converting String Input Into Numbers
+console.log('Now let me ask you to think of two numbers please. ')
+const numberOne = window.prompt('Can you give me the first number ')
+const numberTwo = window.prompt('Now whats the second number? ')
+const firstOperand = Number(numberOne)
+const secondOperand = Number(numberTwo)
+
+//Doing Math
+let sum = firstOperand + secondOperand
+let difference = firstOperand - secondOperand
+let product = firstOperand * secondOperand
+let quotient = firstOperand / secondOperand
+let remainder = firstOperand % secondOperand
+
+console.log(`The sum of ${numberOne} and ${numberTwo} is ${sum} `)
+console.log(`The difference of ${numberOne} and ${numberTwo} is ${difference} `)
+console.log(`The product of ${numberOne} and ${numberTwo} is ${product} `)
+console.log(`The quotient of ${numberOne} and ${numberTwo} is ${quotient} `)
+console.log(
+  `The remainder when you divide ${numberOne} and ${numberTwo} is ${remainder} `
+)
